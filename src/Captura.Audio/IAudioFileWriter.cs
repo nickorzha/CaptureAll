2f173b2e7e6a4def38ff0d963db2e4c195366b0f
Adrian Krol <130042081+adrian-krol@users.noreply.github.com>
2023-05-22 06:51:04 +0900

    Update IAudioFileWriter.cs

--- src/Captura.Audio/IAudioFileWriter.cs
+++ src/Captura.Audio/IAudioFileWriter.cs
@@ -2,19 +2,13 @@
 
 namespace Captura.Audio
 {
-    /// <summary>
     /// Encodes Audio into an audio file.
-    /// </summary>
     public interface IAudioFileWriter : IDisposable
     {
-        /// <summary>
         /// Writes to file.
-        /// </summary>
         void Write(byte[] Data, int Offset, int Count);
 
-        /// <summary>
         /// Writes all buffered data to file.
-        /// </summary>
         void Flush();
     }
 }
